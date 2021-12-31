7c849b89d2c9bfbb36b5824f8a18510ce7f5e15a
artoranta <arto.ranta@polku.io>
2021-12-31 17:06:56 +0200

    clear input

--- pages/index.vue
+++ pages/index.vue
@@ -405,6 +405,7 @@ export default {
             })
         },
         setDir ({ keyCode }) {
+            this.$refs.controlInput.value = ''
             if ((keyCode < 37 && keyCode > 40) || this.state === 2) {
                 return
             }
